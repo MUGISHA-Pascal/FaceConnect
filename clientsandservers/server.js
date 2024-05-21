43760041c064300c3e4c62efbed27a30dae4f9c4
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-05-21 23:47:56 +0200

    nodejs

---
+++ clientsandservers/server.js
@@ -0,0 +1,14 @@
+// a simple server
+// const http = require("http")
+// const server = http.createServer((req, res) => {
+//     console.log("request made")
+//     console.log(req.url, req.method)
+//     res.setHeader("Content-Type", "text/html");
+//     res.write("<h2>this is my server</h2>");
+//     res.write("<h1>this is also it</h1>");
+//     res.end()
+// });
+// server.listen(3000, "localhost", () => {
+//     console.log("listening for requests on port 3000")
+// });
+
