0bf6bc3f6db3845d1e03d9123b711afd5d22c653
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-05-21 21:41:56 +0200

    nodejs

---
+++ jsbasics/files.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+// reading a file
+// fs.readFile('./exe.txt',(err,data)=>{
+// if (err){
+//     console.log(err)
+// }
+// console.log(data.toString())
+// })
+// console.log("last line")
+
+// writing to a file
+// fs.writeFile("./file.txt","this is me , mugisha pascal",()=>{
+//     console.log("finished writing in the file")
+// })
+
+//creating a folder
+// fs.mkdir("./file2",(err)=>{
+//     if (err){
+//         console.log(err);
+//     }
+//     console.log("the folder is created")
+// })
+
+// checking existance of filesystem components
+// if (fs.existsSync("./file2")){
+//     console.log("the file system arleady exists")
+// }else{
+//     console.log("the file system is not found")
+// }
+
+//removing a folder
+// fs.rmdir("./file2",(err)=>{
+//     if (err){
+//         console.log(err)
+//     }
+//     console.log("finished removal")
+// })
+
+
---
+++ jsbasics/globals.js
@@ -0,0 +1,15 @@
+// globals
+
+// console.log(global)
+// setTimeout(()=>{
+//     console.log("in the timeout")
+// },3000)
+
+// const int= setInterval(()=>{
+// console.log("in the interval")
+// },2000)
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(document.querySelector)
