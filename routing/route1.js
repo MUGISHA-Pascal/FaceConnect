8637212816d1da702baaed9fc5b40a755fe0b1df
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-05-31 16:34:39 +0200

    nodejs

--- routing/route1.js
+++ routing/route1.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs");
+app.set("views", "./views");
+app.set("view engine", 'ejs');
+app.get("/file", (req, res) => {
+    res.render("index");
+});
 app.get("/", (req, res) => {
     res.send("this is the first page");
 });
