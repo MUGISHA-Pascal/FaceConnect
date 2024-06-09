b82e810e483d2fc05fe05f91e9f77da3be64f0d0
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-06-10 00:58:49 +0200

    nodejs

--- mongodb/index1.js
+++ mongodb/index1.js
@@ -32,19 +32,43 @@ const mongoose = require("mongoose");
         //     .select({ name: 1, tags: 1 })
         //     console.log("the found results is ",getResults)
         // //  comparison query operators
-//         const course = new Course({
-//             name: "nodejs",
-//                 author: "mugisha",
-//                 tags: ["backend", "API"],
-//                 ispublished: true
-//         })
-//         const results=await course.save()
-//         console.log(results)
-//         const foundResults=await Course.find({
-//             author:{$in :["pascal"]}
-//         })
-// console.log("found results are : ",foundResults)
+        //         const course = new Course({
+        //             name: "nodejs",
+        //                 author: "mugisha",
+        //                 tags: ["backend", "API"],
+        //                 ispublished: true
+        //         })
+        //         const results=await course.save()
+        //         console.log(results)
+        //         const foundResults=await Course.find({
+        //             author:{$in :["pascal"]}
+        //         })
+        // console.log("found results are : ",foundResults)
 
+        // // logical query operator
+        const course1 = new Course({
+            name: "Nodejs",
+            author: "pascal",
+            tags: ["backend", "API"],
+            ispublished: true
+          });
+          const course2 = new Course({
+            name: "Reactjs",
+            author: "pascal",
+            tags: ["props", "components"],
+            ispublished: true
+          });
+      
+          // Save each Course instance separately
+          const results1 = await course1.save();
+          console.log("Course 1 saved:", results1);
+      
+          const results2 = await course2.save();
+          console.log("Course 2 saved:", results2);
+      
+      
+        const foundResults=await Course.find().or([{name:"Reactjs"},{name: "Nodejs"}])
+        console.log("found results : ",foundResults)
     } catch (err) {
         console.error("not connected", err);
     }
