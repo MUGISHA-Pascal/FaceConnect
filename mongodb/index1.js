eb3dbf9e524bd043bd57d9b590ba1299c5fce9be
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-06-10 00:38:50 +0200

    nodejs

--- mongodb/index1.js
+++ mongodb/index1.js
@@ -13,24 +13,37 @@ const mongoose = require("mongoose");
             ispublished: Boolean
         })
         const Course = mongoose.model("Course", courseSchema)
-        const course = new Course({
-            name: "nodejs",
-            author: "pascal",
-            tags: ["backend", "API"],
-            ispublished: true
-        })
-        // inserting document into the database
+        // const course = new Course({
+        //     name: "nodejs",
+        //     author: "pascal",
+        //     tags: ["backend", "API"],
+        //     ispublished: true
+        // })
+        // // inserting document into the database
         // const results = await course.save();
         // console.log(results);
-        // querying document into the database
-        const getResults = await Course.find({
-            name: "nodejs",
-            author: "pascal"
-        })
-            .limit(10)
-            .sort({ name: 1 })
-            .select({ name: 1, tags: 1 })
-            console.log("the found results is ",getResults)
+        // // querying document into the database
+        // const getResults = await Course.find({
+        //     name: "nodejs",
+        //     author: "pascal"
+        // })
+        //     .limit(10)
+        //     .sort({ name: 1 })
+        //     .select({ name: 1, tags: 1 })
+        //     console.log("the found results is ",getResults)
+        // //  comparison query operators
+//         const course = new Course({
+//             name: "nodejs",
+//                 author: "mugisha",
+//                 tags: ["backend", "API"],
+//                 ispublished: true
+//         })
+//         const results=await course.save()
+//         console.log(results)
+//         const foundResults=await Course.find({
+//             author:{$in :["pascal"]}
+//         })
+// console.log("found results are : ",foundResults)
 
     } catch (err) {
         console.error("not connected", err);
