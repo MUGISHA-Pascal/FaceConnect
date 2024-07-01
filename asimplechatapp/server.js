8d8cc5becaefce88d747899fe6015a42cc53e1db
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-01 23:52:39 +0200

    a simple chat app

---
+++ asimplechatapp/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection");
