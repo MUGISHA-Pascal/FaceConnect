9158b9f9aa820f32616e7f104844bad385813867
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-04 21:18:49 +0200

    a simple change

--- authentication/server.js
+++ authentication/server.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const jwt=require("jsonwebtoken");
-const bodyParser=require("");
+const bodyParser=require("body-parser");
 const app=express();
 require("dotenv").config();
 const authroutes=require('./routes/auth');
