7e626ce2a59fae3956da3976ad1593631ca6c098
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-04 21:55:43 +0200

    a file upload program

---
+++ fileupload/server.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const multer=require("multer");
+const path=require("path");
+const port=3000;
+const app=express();
+
+const storage=multer.diskStorage({
+    destination:(req,file,cd)=>{
+        cb(null,"uploads/");
+    }
+},{
+    filename:(req,file,cb)=>{
+        cb(null,Date.now()+path.extname(file.originalname));
+    },
+});
+
+const upload=multer({storage:storage});
+app.use(express.static("public"));
+app.post("/upload",upload.single("file"),(req,res)=>{
+    res.send("file uploaded successfully");
+});
+
+app.listen(port,()=>{
+    console.log(`server is running on http://localhost:${port}`)
+})
