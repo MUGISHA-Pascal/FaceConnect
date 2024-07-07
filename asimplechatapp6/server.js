864cfb1ac5dfd5da13e823111da8952046d98d33
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-07 20:52:04 +0200

    a simple chat app

---
+++ asimplechatapp6/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("app runnning on http://localhost:3000");
+});
